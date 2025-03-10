25675be28496a6f87d59d7bb4dd7e1b585c36f0c
tanikina <tatiana.anikina@dfki.de>
2025-03-10 12:38:03 +0100

    fix default batch size for backtranslation

--- FreeAL/self_training_slm/data_in/backtrans.py
+++ FreeAL/self_training_slm/data_in/backtrans.py
@@ -8,14 +8,14 @@ from transformers import pipeline
 MAX_LENGTH = 512  # 768
 
 # Note that batch_size depends on the model:
-# "Helsinki-NLP/opus-mt-ko-en" can have 16
+# "Helsinki-NLP/opus-mt-ko-en" can have 32
 # "Helsinki-NLP/opus-mt-tc-big-en-ko" needs batch_size 16
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--task", default="massive", help="massive")
 args = parser.parse_args()
-pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-tc-big-ko-en", device=0)
-pipe_bt = pipeline("translation", model="Helsinki-NLP/opus-mt-tc-big-en-ko", device=0)
+pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-de-en", device=0)
+pipe_bt = pipeline("translation", model="Helsinki-NLP/opus-mt-en-de", device=0)
 # pipe_bt = pipeline("translation", model="Helsinki-NLP/opus-mt-en-ko",device=0)
 with open(os.path.join("data_in", args.task, "train_chat_vanilla.csv"), "r") as file:
     reader = csv.reader(file)
@@ -28,20 +28,20 @@ for row in data:
 results = []
 results_bt = []
 
-for i in range(len(ori_text) // 16 + 1):
+for i in range(len(ori_text) // 32 + 1):
     print(i)
     tmp_text = (
-        ori_text[i * 16 : (i + 1) * 16] if (i + 1) * 16 < len(ori_text) else ori_text[i * 16 :]
+        ori_text[i * 32 : (i + 1) * 32] if (i + 1) * 32 < len(ori_text) else ori_text[i * 32 :]
     )
-    batch_result = pipe(tmp_text, batch_size=16, max_length=MAX_LENGTH)
+    batch_result = pipe(tmp_text, batch_size=32, max_length=MAX_LENGTH)
     mid_text = [tmp_result["translation_text"] for tmp_result in batch_result]
     results += mid_text
 
 
-for i in range(len(results) // 16 + 1):
+for i in range(len(results) // 32 + 1):
     print(i)
-    tmp_text = results[i * 16 : (i + 1) * 16] if (i + 1) * 16 < len(results) else results[i * 16 :]
-    batch_result = pipe_bt(tmp_text, batch_size=16, max_length=MAX_LENGTH)
+    tmp_text = results[i * 32 : (i + 1) * 32] if (i + 1) * 32 < len(results) else results[i * 32 :]
+    batch_result = pipe_bt(tmp_text, batch_size=32, max_length=MAX_LENGTH)
     bt_text = [tmp_result["translation_text"] for tmp_result in batch_result]
     results_bt += bt_text
 
