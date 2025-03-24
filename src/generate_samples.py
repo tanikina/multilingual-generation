40b01ed1091fe016aa4126f8f89585a9dee4c9da
tanikina <tatiana.anikina@dfki.de>
2025-03-24 13:54:15 +0100

    use hf_token also with tokenizer

--- src/generate_samples.py
+++ src/generate_samples.py
@@ -249,7 +249,7 @@ def generate_demos(args):
             "Unsupported model name {model_name}. Should be either Llama, Qwen or Aya model."
         )
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, token=HF_TOKEN)
     tokenizer.pad_token_id = tokenizer.eos_token_id
     model.config.pad_token_id = tokenizer.pad_token_id
 
