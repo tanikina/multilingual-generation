a38addad2f0cf508e84f5558c9cbab248e31b90d
tanikina <tatiana.anikina@dfki.de>
2025-03-27 20:21:42 +0100

    add scripts for massive-100 with gemma3_4b

--- scripts/massive10/gemma3_4b/generate_te-IN.sh
+++ scripts/massive10/gemma3_4b/generate_te-IN.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo only_summarized_intent
 python src/generate_samples.py \
---language=de-DE \
---input_path=data/de-massive/de-DE_train.csv \
---output_path=data/generated/massive10/gemma3_4b/de-DE_only_summarized_intent.csv \
+--language=te-IN \
+--input_path=data/de-massive/te-IN_train.csv \
+--output_path=data/generated/massive10/gemma3_4b/te-IN_only_summarized_intent.csv \
 --dataset=massive10 \
 --model_name=google/gemma-3-4b-it \
 --num_samples_to_generate=100 \
@@ -12,9 +12,9 @@ python src/generate_samples.py \
 --use_vllm=True
 echo summarized_intent_with_10_target_lang_demos
 python src/generate_samples.py \
---language=de-DE \
---input_path=data/de-massive/de-DE_train.csv \
---output_path=data/generated/massive10/gemma3_4b/de-DE_summarized_intent_with_10_target_lang_demos.csv \
+--language=te-IN \
+--input_path=data/de-massive/te-IN_train.csv \
+--output_path=data/generated/massive10/gemma3_4b/te-IN_summarized_intent_with_10_target_lang_demos.csv \
 --dataset=massive10 \
 --model_name=google/gemma-3-4b-it \
 --num_samples_to_generate=100 \
@@ -23,9 +23,9 @@ python src/generate_samples.py \
 --use_vllm=True
 echo summarized_intent_with_10_english_demos
 python src/generate_samples.py \
---language=de-DE \
---input_path=data/de-massive/de-DE_train.csv \
---output_path=data/generated/massive10/gemma3_4b/de-DE_summarized_intent_with_10_english_demos \
+--language=te-IN \
+--input_path=data/de-massive/te-IN_train.csv \
+--output_path=data/generated/massive10/gemma3_4b/te-IN_summarized_intent_with_10_english_demos \
 --dataset=massive10 \
 --model_name=google/gemma-3-4b-it \
 --num_samples_to_generate=100 \
@@ -35,9 +35,9 @@ python src/generate_samples.py \
 --use_vllm=True
 echo summarized_intent_with_10_target_lang_demos_and_revision
 python src/generate_samples.py \
---language=de-DE \
---input_path=data/de-massive/de-DE_train.csv \
---output_path=data/generated/massive10/gemma3_4b/de-DE_summarized_intent_with_10_target_lang_demos_and_revision \
+--language=te-IN \
+--input_path=data/de-massive/te-IN_train.csv \
+--output_path=data/generated/massive10/gemma3_4b/te-IN_summarized_intent_with_10_target_lang_demos_and_revision \
 --dataset=massive10 \
 --model_name=google/gemma-3-4b-it \
 --num_samples_to_generate=100 \
