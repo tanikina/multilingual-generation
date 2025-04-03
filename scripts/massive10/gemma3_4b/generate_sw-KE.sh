dd5623845249369731a2b47226ad05d2ed727b04
Ubuntu <ubuntu@nlp.lxd>
2025-04-03 21:22:16 +0000

    fix paths for gemma3_4b

--- scripts/massive10/gemma3_4b/generate_sw-KE.sh
+++ scripts/massive10/gemma3_4b/generate_sw-KE.sh
@@ -2,7 +2,7 @@
 echo only_summarized_intent
 python src/generate_samples.py \
 --language=sw-KE \
---input_path=data/de-massive/sw-KE_train.csv \
+--input_path=data/sw-massive/sw-KE_train.csv \
 --output_path=data/generated/massive10/gemma3_4b/sw-KE_only_summarized_intent.csv \
 --dataset=massive10 \
 --model_name=google/gemma-3-4b-it \
@@ -13,7 +13,7 @@ python src/generate_samples.py \
 echo summarized_intent_with_10_target_lang_demos
 python src/generate_samples.py \
 --language=sw-KE \
---input_path=data/de-massive/sw-KE_train.csv \
+--input_path=data/sw-massive/sw-KE_train.csv \
 --output_path=data/generated/massive10/gemma3_4b/sw-KE_summarized_intent_with_10_target_lang_demos.csv \
 --dataset=massive10 \
 --model_name=google/gemma-3-4b-it \
@@ -24,7 +24,7 @@ python src/generate_samples.py \
 echo summarized_intent_with_10_english_demos
 python src/generate_samples.py \
 --language=sw-KE \
---input_path=data/de-massive/sw-KE_train.csv \
+--input_path=data/sw-massive/sw-KE_train.csv \
 --output_path=data/generated/massive10/gemma3_4b/sw-KE_summarized_intent_with_10_english_demos.csv \
 --dataset=massive10 \
 --model_name=google/gemma-3-4b-it \
@@ -36,7 +36,7 @@ python src/generate_samples.py \
 echo summarized_intent_with_10_target_lang_demos_and_revision
 python src/generate_samples.py \
 --language=sw-KE \
---input_path=data/de-massive/sw-KE_train.csv \
+--input_path=data/sw-massive/sw-KE_train.csv \
 --output_path=data/generated/massive10/gemma3_4b/sw-KE_summarized_intent_with_10_target_lang_demos_and_revision.csv \
 --dataset=massive10 \
 --model_name=google/gemma-3-4b-it \
